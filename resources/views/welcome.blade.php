ae92e4b0a550c0f007774b9a32f54d95b44ab564
thomas-fan <youxiboy007@vip.qq.com>
2017-09-29 11:19:25 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelabc
                 </div>
 
                 <div class="links">
